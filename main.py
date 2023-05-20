d726bbc222b5d2a756c35915ef9be6e8d06c640a
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2023-05-19 23:32:26 -0400

    fixed turtle creation

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # Imports
 from turtle import Turtle, Screen
 
-# Create turtle and screen
-t1 = Turtle()
+# Create screen
 screen = Screen()
 
 # Set width and height of screen
@@ -11,6 +10,16 @@ screen.setup(width=500, height=400)
 # Prompt user on turtle bet
 screen.textinput(title="Make your bet!", prompt="Turtles are colors of the rainbow. Enter a color: ")
 
+# Create turtles and tell them to 'goto' their spots ;)
+def create():
+    # First turtle
+    t1 = Turtle()
+    t1.shape("turtle")
+    t1.color("red")
+    t1.penup()
+    t1.goto(x=-230,y=-90)
+
+create()
 
 # Exit on click
 screen.exitonclick()
