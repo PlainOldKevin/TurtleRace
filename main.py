fffe2845f5fc1c68546e2af3be676f51819be438
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2023-05-19 23:10:23 -0400

    screen size and user prompt

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from turtle import Turtle, Screen
 t1 = Turtle()
 screen = Screen()
 
+# Set width and height of screen
+screen.setup(w=500, h=400)
+
+# Prompt user on turtle bet
+screen.textinput(title="Make your bet!", prompt="Turtles are colors of the rainbow. Enter a color: ")
 
 
 # Exit on click
