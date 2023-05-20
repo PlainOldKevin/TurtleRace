1a2d4e39a6f7f87ac98e04ce5f682d7b6f739b76
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2023-05-19 23:42:07 -0400

    created turtle line of different colors

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ screen.setup(width=500, height=400)
 # Prompt user on turtle bet
 screen.textinput(title="Make your bet!", prompt="Turtles are colors of the rainbow. Enter a color: ")
 
-# Create turtles and tell them to 'goto' their spots ;)
-def create():
-    # First turtle
-    t1 = Turtle("turtle")
-    t1.color("red")
-    t1.penup()
-    t1.goto(x=-230,y=-90)
+# Create turtles and place along starting line
+# Turtle colors
+turtle_colors =["red", "orange", "yellow", "green", "blue", "purple"]
+# Starting y position for relation to each other
+starty = -90
+# For loop to make 'em
+for i in range(6):
+    t = Turtle("turtle")
+    t.color(turtle_colors[i])
+    t.penup()
+    # Goto here
+    t.goto(-230, starty)
+    # Decrese starty each loop for a line of turtles
+    starty += 30
 
-create()
 
 # Exit on click
 screen.exitonclick()
